f1433a53a19d418677c873d034cd8cf73d72f95f
hashwavelab <hashwavelab@gmail.com>
2022-03-20 15:46:59 +0000

    ethclient: impl SubscribePendingTransactions

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -311,6 +311,12 @@ func (ec *Client) SubscribeNewHead(ctx context.Context, ch chan<- *types.Header)
 	return ec.c.EthSubscribe(ctx, ch, "newHeads")
 }
 
+// SubscribePendingTransactions subscribes to notifications about the hash of pending transactions
+// on the given channel.
+func (ec *Client) SubscribePendingTransactions(ctx context.Context, ch chan<- common.Hash) (ethereum.Subscription, error) {
+	return ec.c.EthSubscribe(ctx, ch, "newPendingTransactions")
+}
+
 // State Access
 
 // NetworkID returns the network ID (also known as the chain ID) for this chain.
